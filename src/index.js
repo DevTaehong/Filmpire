91d95bc7f203dd626a4925c03e8d03a96fab3ec6
DevTaehong <minth1123@icloud.com>
2023-06-14 23:55:07 -0300

    Add Avatar and dark mode button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import App from './components/App.jsx';
 
+const theme = createTheme({});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  // How to theme: https://mui.com/material-ui/customization/theming/
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
